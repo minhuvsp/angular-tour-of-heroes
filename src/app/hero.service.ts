fe05b417de439b5a730ce68e0b8dfc3fb540928e
Min Hu <minhu@vsp.com>
2020-05-21 21:46:55 -0700

    updates

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,16 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-import { MessageService } from './message.service';
+import {Injectable} from '@angular/core';
 
+import {Observable, of} from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+import {MessageService} from './message.service';
+
+@Injectable({providedIn: 'root'})
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
-
-  // getHeroes(): Hero[] {
-  //   return HEROES;
-  // }
-
-  // getHeroes(): Observable<Hero[]> {
-  //   return of(HEROES);
-  // }
+  constructor(private messageService: MessageService) {
+  }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
@@ -26,4 +18,9 @@ export class HeroService {
     return of(HEROES);
   }
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
